8dbc8e0c4b6de2667b3ca684f984876305a9aed4
Grzegorz Nowak <strange3studio@gmail.com>
2019-09-23 17:59:10 +0200

    ffmpeg installation IWP
    Former-commit-id: 3348f64862feb31c311c58a729b5eef09db18e4e

--- roles/ffmpeg/files/audio_convert_installation.sh
+++ roles/ffmpeg/files/audio_convert_installation.sh
@@ -41,6 +41,11 @@ cd ffmpeg
 #make distclean
 #hash -r
 
+hg clone https://bitbucket.org/multicoreware/x265
+cd x265/build/linux
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source && PATH="$HOME/bin:$PATH"
+make && make install
+
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" \
    ./configure \
   --prefix="$HOME/ffmpeg_build" \
